5048d90af3bb04967aff1d692735da4ceaa30f52
Akhmadjon <ahmadjonzuhriddinov84@gmail.com>
2022-11-20 23:41:01 +0900

    added some bootstrap for view

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,16 +3,18 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email','username','first_name','last_name','age','is_staff']
-    fieldsets = UserAdmin.fieldsets +(
+    list_display = ['email', 'username', 'first_name', 'last_name', 'age', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('age',)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None,{'fields':('age',)}),
+        (None, {'fields': ('age',)}),
     )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ## external app
+    'crispy_forms',
+    ## apps I created
     'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
