c33088e91bdae318c2bc79451f2dd612db7b68c9
Akhmadjon <ahmadjonzuhriddinov84@gmail.com>
2022-11-26 20:29:39 +0900

    Text editor fields changed and CKEDITOR installed

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     ## external app
     'crispy_forms',
+    'ckeditor',
+    'ckeditor_uploader',
     ## apps I created
     'accounts',
     'pages',
@@ -118,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
@@ -144,3 +149,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 ## MEDIA
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('models'))
+
+#CKEDITOR CONFIGS
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Full',
+    },
+}
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_RESTRICT_BY_USER = True
